072a701618d90dcdb20d6eaf75e00df3e0f38e69
Gregory Jean Baptiste <gregster0@gmail.com>
2023-11-14 17:36:02 -0500

    puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { useState} from 'react'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
     <>
       <div>
+        { 
+          puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
